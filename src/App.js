be2b5237cbcbdfe99fef08bfd240ddceb7cfce76
Jorge Salazar <j-salazarp@hotmail.com>
2021-06-08 21:40:00 +0200

    Creamos el componente sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouseUser,faUser,faSearch, faCity } from '@fortawesome/free-solid-svg-icons';
+import Sidebar from './components/layout/Sidebar';
+
+
 
 import './reset.css';
 import './App.css';
@@ -11,80 +12,7 @@ function App() {
 
     <React.Fragment>
 
-      <section id="sidebar">
-          <div className="title-container">
-            <img src="/Logo-Meep.png" alt="Meep app"/>
-            <h1>Meep App</h1>
-          </div>
-          <div className="options-container">
-              <div className="options">            
-                  <FontAwesomeIcon icon={faHouseUser} className="icon"/>
-                  <span>Home</span>
-              </div>
-              <div className="options">
-                  <FontAwesomeIcon icon={faUser} className="icon"/>
-                  <span>User</span>
-              </div>
-          
-              <div className="options">
-                  <FontAwesomeIcon icon={faSearch} className="icon"/>
-                  <span>Search</span>
-
-                
-              </div>
-
-              <div className="container-search">
-                  <form action="#">
-                    <section className="input-box"> 
-                    <span>Name</span>
-                      <input 
-                            type="text" 
-                        
-                      />
-                    </section>
-                    <section className="battery-level">
-                          <span>Battery Level</span>
-                          <input 
-                              type="checkbox" 
-                              id="low-battery" 
-                            
-                          />
-                          <label htmlFor="low-battery"> {'< 25%'}</label>
-                          <input 
-                              type="checkbox" 
-                              id="medium-battery" 
-                              
-                          />
-                          <label htmlFor="medium-battery"> 25% - 50% </label>
-                          <input 
-                              type="checkbox" 
-                              id="high-battery" 
-                              
-                          />
-                          <label htmlFor="high-battery"> {'> 50%'} </label>
-                      </section>
-
-                      <input
-                        type="submit"
-                       
-                      />
-                  </form>
-                
-              </div>
-
-          </div>    
-
-          <section className="options-container other-cities">
-              <div className="options">            
-                  <FontAwesomeIcon icon={faCity} className="icon"/>
-                  <span>Other cities</span>
-                  
-
-              </div>
-          </section>
-
-      </section>
-
+      <Sidebar/>
       <main id="dashboard-content">
 
         <section id="container-header">
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser,faUser,faSearch, faCity } from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => {
+
+    return (
+
+        
+      <section id="sidebar">
+        <div className="title-container">
+          <img src="/Logo-Meep.png" alt="Meep app" />
+          <h1>Meep App</h1>
+        </div>
+        <div className="options-container">
+          <div className="options">
+            <FontAwesomeIcon icon={faHouseUser} className="icon" />
+            <span>Home</span>
+          </div>
+          <div className="options">
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            <span>User</span>
+          </div>
+
+          <div className="options">
+            <FontAwesomeIcon icon={faSearch} className="icon" />
+            <span>Search</span>
+          </div>
+
+          <div className="container-search">
+            <form action="#">
+              <section className="input-box">
+                <span>Name</span>
+                <input type="text" />
+              </section>
+              <section className="battery-level">
+                <span>Battery Level</span>
+                <input type="checkbox" id="low-battery" />
+                <label htmlFor="low-battery"> {"< 25%"}</label>
+                <input type="checkbox" id="medium-battery" />
+                <label htmlFor="medium-battery"> 25% - 50% </label>
+                <input type="checkbox" id="high-battery" />
+                <label htmlFor="high-battery"> {"> 50%"} </label>
+              </section>
+
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
+
+        <section className="options-container other-cities">
+          <div className="options">
+            <FontAwesomeIcon icon={faCity} className="icon" />
+            <span>Other cities</span>
+          </div>
+        </section>
+      </section>
+    );
+}
+ 
+export default Sidebar;
