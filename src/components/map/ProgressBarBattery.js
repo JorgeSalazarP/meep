f02793e4bb697c0d6dccfc31f0a2521100c099d8
Jorge Salazar <j-salazarp@hotmail.com>
2021-06-13 13:08:24 +0200

    Controlamos con styled components el color del nivel de batería.

--- src/components/map/ProgressBarBattery.js
+++ src/components/map/ProgressBarBattery.js
@@ -7,9 +7,14 @@ const ProgressBarBattery = styled.span`
     border-radius: 3px;
     box-shadow: 0 1px 0 rgba(255, 255, 255, .5) inset;
     transition: width .4s ease-in-out; 
-    background-color: ${({ batteryLevel }) => batteryLevel <= rangeBatteryLevel.med ? 'rgb(243, 35, 35)' : 'rgb(132, 235, 15)'  };
-    width: ${({ batteryLevel }) => batteryLevel <= rangeBatteryLevel.med ? '20px' : '50px'  }
 
+    background-color: ${({ batteryLevel }) => batteryLevel <= rangeBatteryLevel.med && 'rgb(243, 35, 35)' }; 
+    background-color: ${({ batteryLevel }) => batteryLevel > rangeBatteryLevel.max && 'rgb(132, 235, 15)' };
+    background-color: ${({ batteryLevel }) => ((batteryLevel > rangeBatteryLevel.med &&  batteryLevel <= rangeBatteryLevel.max) && '#fd9253' )};
+    
+    width: ${({ batteryLevel }) => batteryLevel <= rangeBatteryLevel.med && '20px'};
+    width: ${({ batteryLevel }) => ((batteryLevel > rangeBatteryLevel.med &&  batteryLevel <= rangeBatteryLevel.max) && '30px')};
+    width: ${({ batteryLevel }) => batteryLevel > rangeBatteryLevel.max && '50px'};
     
 `;
 
